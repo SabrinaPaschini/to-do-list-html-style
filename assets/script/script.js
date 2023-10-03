13d7c26c9c7f3e8ff9a6c1ace251a52c7811c64c
Sabrina Paschini <sabrina.paschini@hotmail.com>
2023-10-03 17:46:17 -0300

    my-to-do-list

---
+++ assets/script/script.js
@@ -0,0 +1,37 @@
+// Pegar elementos html
+let inputTxt = document.getElementById('txt-1');
+let btnSend = document.getElementById('btn-send');
+let lista = document.getElementById('lista1')
+let itemLista = document.getElementById('item1');
+
+console.log(itemLista);
+
+btnSend.addEventListener('click', function (e) {
+
+    e.preventDefault(); // Impede o envio do formulário padrão
+
+    let value = inputTxt.value; // Captura o valor do campo de entrada de texto
+
+    // pegar elemento html 
+    
+    
+
+ 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
